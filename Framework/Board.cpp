258259fbbaeb74b8539f8632d8f64f0d0d0a2868
BEN SASSI rached <bensassi@informatique.univ-paris-diderot.fr>
2016-12-08 17:13:56 +0100

    Functions of Board class moved to a cpp file.

---
+++ Framework/Board.cpp
@@ -0,0 +1,35 @@
+using namespace std;
+
+
+template <class T>
+Board<T>::Board():matrix_length_x(), matrix_length_y(), plateau(){};
+
+template <class T>
+Board<T>::Board(int a, int b): matrix_length_x(a), matrix_length_y(b), plateau(matrix_length_x,vector<Box<T>>(matrix_length_y) ){}
+
+template <typename T>
+int Board<T>::get_matrix_length_x(){
+  return matrix_length_x;
+}
+
+template <typename T>
+int Board<T>::get_matrix_length_y(){
+  return matrix_length_y;
+}
+
+
+template <typename T>
+void Board<T>::print(){
+  for(int i = 0; i < matrix_length_x; i++){
+    if(i>0)
+      cout << endl;
+    for(int j = 0; j < matrix_length_y; j++)
+      cout << plateau[i][j];
+  }
+  cout << endl;
+}
+
+template <typename T>
+std::vector <std::vector<Box<T>>>& Board<T>::get_plateau(){
+  return this->plateau;
+}
--- Framework/Board.hpp
+++ Framework/Board.hpp
@@ -1,7 +1,9 @@
-#include "Box.hpp"
+#ifndef __BOARD_HPP__
+#define __BOARD_HPP__
+
 #include <iostream>
 #include <vector>
-using namespace std;
+#include "Box.hpp"
 
 template <typename T>
 class Board : public Box<T>{
@@ -16,38 +18,9 @@ public:
 private:
   const int matrix_length_x;
   const int matrix_length_y;
-  vector <vector<Box<T>>> plateau;
+  std::vector <std::vector<Box<T>>> plateau;
 };
 
-template <class T>
-Board<T>::Board():matrix_length_x(), matrix_length_y(), plateau(){};
+#include "Board.cpp"
 
-template <class T>
-Board<T>::Board(int a, int b): matrix_length_x(a), matrix_length_y(b), plateau(matrix_length_x,vector<Box<T>>(matrix_length_y) ){}
-
-template <typename T>
-int Board<T>::get_matrix_length_x(){
-  return matrix_length_x;
-}
-
-template <typename T>
-int Board<T>::get_matrix_length_y(){
-  return matrix_length_y;
-}
-
-
-template <typename T>
-void Board<T>::print(){
-  for(int i = 0; i < matrix_length_x; i++){
-    if(i>0)
-      cout << endl;
-    for(int j = 0; j < matrix_length_y; j++)
-      cout << plateau[i][j];
-  }
-  cout << endl;
-}
-
-template <typename T>
-std::vector <std::vector<Box<T>>>& Board<T>::get_plateau(){
-  return this->plateau;
-}
+#endif
