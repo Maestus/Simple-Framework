ad42e2d60ceba84e36a58c4aa2b901d4b7a94c8b
BEN SASSI rached <bensassi@informatique.univ-paris-diderot.fr>
2017-01-02 21:12:41 +0100

    Adding sokoban.

--- InitGame.cpp
+++ InitGame.cpp
@@ -2,6 +2,7 @@
 #include "2048_Module/ThreeAndFiveTiles/TwoThreeFiveTiles.hpp"
 #include "2048_Module/NegativeTiles/NegativeTiles.hpp"
 #include "Taquin/Taquin.hpp"
+#include "Sokoban/Sokoban.hpp"
 #include <stdio.h>
 #include <string.h>
 #include "Graphic_mod/InitGraphic.hpp"
@@ -42,11 +43,9 @@ int main(int argc, char const *argv[]) {
   bool instantiation = false;
   bool graphic_mod = false;
   if(argc == 5 && strcmp(argv[4],"--graphic") == 0){
-    cout << "ok" << endl;
     graphic_mod = true;
   }
   if(argc >= 4){
-    cout << "ok" << endl;
     game_name = argv[1];
     const string & _x = argv[2];
     const string & _y = argv[3];
@@ -98,6 +97,36 @@ int main(int argc, char const *argv[]) {
       if(graphic_mod){
         a->init();
         InitGraphic<Taquin> graphic(a);
+        graphic.init_graphic_mode();
+      } else {
+        a->init();
+        a->print();
+        cout << "Joueur computer ? [Y|N] : ";
+        string computer;
+        cin >> computer;
+        if(computer ==  "Y"){
+          a->set_enable_computer_play();
+        }
+        while(!a->has_win() && !a->has_lose()){
+          Direction choice = Direction::NOTKNOWN;
+          choice = a->do_one_move();
+          if(!(choice == Direction::NOTKNOWN)){
+            a->apply_move(choice);
+            cout << BORDURE << endl;
+            a->print();
+            if(a->has_win()){
+              cout << "~~~~~~~~~~~~~~~~ You Win ! ~~~~~~~~~~~~~~~~~~~" << endl;
+            } else if(a->has_lose()) {
+              cout << "~~~~~~~~~~~~~~~~ You Lose ! ~~~~~~~~~~~~~~~~~~~" << endl;
+            }
+          }
+        }
+      } delete a;
+    }else if(game_name == "Sokoban"){
+      Sokoban *a = new Sokoban(x,y);
+      if(graphic_mod){
+        a->init();
+        InitGraphic<Sokoban> graphic(a);
         graphic.init_graphic_mode();
       } else {
         a->init();
